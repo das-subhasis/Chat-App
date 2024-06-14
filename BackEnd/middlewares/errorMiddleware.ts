af2a3aa7fc635343e25d66bd3298f8547a41d961
Subhasis Das <das.shubasis2003@gmail.com>
2024-06-14 17:21:58 +0530

    fixed handling issues

--- BackEnd/middlewares/errorMiddleware.ts
+++ BackEnd/middlewares/errorMiddleware.ts
@@ -1,19 +1,18 @@
-import { NextFunction, Request, Response } from "express"
+import { Request, Response, NextFunction } from "express";
 
-const notFoundHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('inside notFoundHandler method');
+// Middleware to handle 404 errors
+export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
-}
+};
 
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+// General error handling middleware
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode);
     res.json({
         message: err.message,
-        stack: process.env.NODE_ENV === "production" ? null : err.stack
-    })
-}
-
-export { notFoundHandler, errorHandler }
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+};
